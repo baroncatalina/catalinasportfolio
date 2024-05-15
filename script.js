d967103de7fc80a9b214089735c90d199df2c587
baroncatalina <baroncatalinatam@gmail.com>
2024-05-15 14:34:19 -0300

    links

--- script.js
+++ script.js
@@ -19,6 +19,28 @@ document.addEventListener('DOMContentLoaded', function () {
         event.preventDefault();
         var contactBox = document.getElementById('contact-box');
         contactBox.style.display = 'none';
-    })
+    });
+
+    var myName = document.getElementById('nombre-link');
+    myName.addEventListener('click', function (event) {
+        event.preventDefault();
+        var myPictureBox = document.getElementById('my-picture-box');
+        if (myPictureBox.style.display === 'block') {
+            myPictureBox.style.display = 'none';
+            // Hide the box if it's already visible
+        } else {
+            myPictureBox.style.display = 'block'; // Show the box if it's hidden
+        }
+    });
+
+    var closeButton2 = document.getElementById("close-button2");
+
+    closeButton2.addEventListener('click', function (event) {
+        event.preventDefault();
+        var myPictureBox = document.getElementById('my-picture-box');
+        myPictureBox.style.display = 'none';
+    });
+
+
 
 });
